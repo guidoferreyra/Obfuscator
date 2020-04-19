540c733123788f70926d5b913105a64bcc5831db
Rainer Scheichelbauer <res@glyphsapp.com>
2020-04-19 22:52:00 +0200

    Default to built-in .notdef

--- Obfuscator.glyphsFilter/Contents/Resources/plugin.py
+++ Obfuscator.glyphsFilter/Contents/Resources/plugin.py
@@ -30,46 +30,73 @@ class Obfuscator(FilterWithoutDialog):
 		self.keyboardShortcut = None # keyboard Shortcut
 
 	@objc.python_method
+	def notdefLayer( self ):
+		try:
+			from Foundation import NSBundle, NSClassFromString, NSDictionary
+			bundle = NSBundle.bundleForClass_(NSClassFromString("GSExportInstanceOperation"))
+			if bundle:
+				path = bundle.pathForResource_ofType_("notDef","plist")
+				layerDict = NSDictionary.dictionaryWithContentsOfFile_(path)
+				layer = GSLayer.alloc().initWithDict_format_(layerDict,1)
+				return layer
+			return None
+		except Exception as e:
+			import traceback
+			print(traceback.format_exc())
+			self.logToConsole( "notdefLayer: %s" % str(e) )
+			return None
+			
+
+	@objc.python_method
 	def filter(self, layer, inEditView, customParameters):
 
 		if len(customParameters) !=0:
 			character = customParameters['char']
 		else:
-			character = 'apple'
+			character = '.notdef'
 
 		fuente = layer.parent.parent
 		glyphOrigen = fuente.glyphs[character]
 		if glyphOrigen is None:
-			msg="Please add a glyph with the name: \"%s\" or set the correct name in the Filter parameter" % character
 			if inEditView:
-				Message(title="Missing Glyph", message=msg)
-			else:
-				print("Filter ‘%s’: %s" % (self.menuName, msg))
-			return
-			
-		# find origin layer:
-		layerOrigen = glyphOrigen.layers[layer.layerId]
-		if layerOrigen is None:
-			layerOrigen = glyphOrigen.layers[layer.master.id]
+				# brings macro window to front and clears its log:
+				Glyphs.clearLog()
+				Glyphs.showMacroWindow()
+			msg="Could not find a glyph with the name: ‘%s’. Defaulting to built-in .notdef." % character
+			print("Filter ‘%s’: %s" % (self.menuName, msg))
+			# fallback .notdef:
+			layerOrigen = self.notdefLayer()
+		else:
+			# find origin layer:
+			layerOrigen = glyphOrigen.layers[layer.layerId]
+			if layerOrigen is None:
+				layerOrigen = glyphOrigen.layers[layer.master.id]
 		
-		# delete contents of target layer:
-		layer.clear()
+		if not layerOrigen:
+			print("")
+		else:
+			# delete contents of target layer:
+			layer.clear()
 
-		# duplicate shapes into target layer:
-		try:
-			# GLYPHS 3
-			for newShape in layerOrigen.shapes:
-				layer.shapes.append(newShape.copy())
-		except:
-			# GLYPHS 2
-			for newPath in layerOrigen.paths:
-				layer.paths.append(newPath.copy())
+			# duplicate shapes into target layer:
+			try:
+				# GLYPHS 3
+				for newShape in layerOrigen.shapes:
+					layer.shapes.append(newShape.copy())
+			except:
+				# GLYPHS 2
+				for newPath in layerOrigen.paths:
+					layer.paths.append(newPath.copy())
 		
-		# copy spacing and (group) kerning:
-		layer.width = layerOrigen.width
-		glyph = layer.parent
-		glyph.leftKerningGroup = glyphOrigen.leftKerningGroup
-		glyph.rightKerningGroup = glyphOrigen.rightKerningGroup
+			# copy spacing and (group) kerning:
+			layer.width = layerOrigen.width
+			glyph = layer.parent
+			if glyphOrigen:
+				glyph.leftKerningGroup = glyphOrigen.leftKerningGroup
+				glyph.rightKerningGroup = glyphOrigen.rightKerningGroup
+			else:
+				glyph.leftKerningGroup = None
+			glyph.rightKerningGroup = None
 	
 	@objc.python_method
 	def __file__(self):
